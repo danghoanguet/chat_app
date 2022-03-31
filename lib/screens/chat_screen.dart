3e0d0835ec78dd3fa0e309c60a5b1aefc020b7d0
danghoanguet <18020541@vnu.edu.vn>
2022-03-31 15:11:07 +0700

    authentication with firebase, send message to firestore database with uid, fetch data and show

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../widgets/chat/messages.dart';
+import '../widgets/chat/new_messages.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat Screen'),
+        actions: [
+          DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(Icons.exit_to_app, color: Colors.pink),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text('Logout'),
+                      ],
+                    ),
+                  ),
+                  value: 'logout',
+                ),
+              ],
+              onChanged: (value) {
+                if (value == 'logout') FirebaseAuth.instance.signOut();
+              })
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+            NewMessages(),
+          ],
+        ),
+      ),
+    );
+  }
+}
